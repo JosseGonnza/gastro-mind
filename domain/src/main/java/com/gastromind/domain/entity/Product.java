46e4098459ecd757739d9608ca7548532c1d4918
JosseGonnza <accion.quevedo@gmail.com>
2026-01-14 09:11:14 +0000

    🟢 test(tdd): create a valid product

---
+++ domain/src/main/java/com/gastromind/domain/entity/Product.java
@@ -0,0 +1,61 @@
+package com.gastromind.domain.entity;
+
+import com.gastromind.domain.valueobject.Allergen;
+import com.gastromind.domain.valueobject.Category;
+import com.gastromind.domain.valueobject.UnitOfMeasure;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class Product {
+    private final UUID id;
+    private final String name;
+    private final String description;
+    private final Category category;
+    private final UnitOfMeasure unit;
+    private final Set<Allergen> allergens;
+
+    public Product(UUID id, String name, String description, Category category, UnitOfMeasure unit, Set<Allergen> allergens) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.unit = unit;
+        this.allergens = allergens;
+    }
+
+    public static Product create(String name, String description, Category category, UnitOfMeasure unit, Set<Allergen> allergens) {
+        return new Product(
+                UUID.randomUUID(),
+                name,
+                description,
+                category,
+                unit,
+                allergens
+        );
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public UnitOfMeasure getUnit() {
+        return unit;
+    }
+
+    public Set<Allergen> getAllergens() {
+        return allergens;
+    }
+}

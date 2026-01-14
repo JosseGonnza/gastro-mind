855c47063b052332040fbb302ec24166c3aa0aca
JosseGonnza <accion.quevedo@gmail.com>
2026-01-14 12:36:52 +0000

    🔴 test(tdd): create a valid batch

---
+++ domain/src/test/java/com/gastromind/domain/entity/BatchTest.java
@@ -0,0 +1,36 @@
+package com.gastromind.domain.entity;
+
+import com.gastromind.domain.valueobject.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Batch debería")
+public class BatchTest {
+
+    private static final Product PRODUCT = Product.create(
+            "Harina",
+            "Harina de Trigo",
+            Category.GRAIN,
+            UnitOfMeasure.KILOGRAM,
+            Set.of(Allergen.GLUTEN));
+    private static final String SKU = "LOT-2026-001";
+    private static final LocalDate EXPIRATION_DATE = LocalDate.now().plusMonths(6);
+    private static final Money PURCHASE_PRICE = Money.of(50.00);
+    private static final Quantity INITIAL_QUANTITY = Quantity.of(25.0);
+
+    @Test
+    @DisplayName("Crear un lote válido con stock inicial igual que el actual")
+    void shouldCreateValidBatch() {
+        Batch batch = Batch.create(PRODUCT, SKU, EXPIRATION_DATE, PURCHASE_PRICE, INITIAL_QUANTITY);
+
+        assertThat(batch.getId()).isNotNull();
+        assertThat(batch.getSku()).isEquals(SKU);
+        assertThat(batch.getCurrentQuantity().value()).isEquals(25.0);
+        assertThat(batch.getEntryDate()).isToday();
+    }
+}

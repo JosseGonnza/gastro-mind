21cdb59e3cd0f0625f23432ea03ca7acbc221965
JosseGonnza <accion.quevedo@gmail.com>
2026-01-16 19:02:13 +0000

    💫 feat: add Difficulty enum for recipe

---
+++ domain/src/main/java/com/gastromind/domain/valueobject/Difficulty.java
@@ -0,0 +1,7 @@
+package com.gastromind.domain.valueobject;
+
+public enum Difficulty {
+    EASY,       // Recetas básicas (ensaladas, tostadas)
+    MEDIUM,     // Elaboraciones estándar (guisos, arroces)
+    HARD        // Alta cocina, técnicas avanzadas
+}
